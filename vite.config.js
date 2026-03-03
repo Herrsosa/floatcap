c6dfe067fa8ed5b97b627221b081813f0aa3ccca
Herrsosa <nilshertzner@hotmail.de>
2026-03-03 13:19:36 +0000

    Update article 2: Implement diagram prompts as HTML/SVG

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
   build: {
     rollupOptions: {
       input: {
